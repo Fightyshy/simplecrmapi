f2c907232d8ae3b81ff5255a9c0b2dec9abc6ead
Fightyshy <tharonn@gmail.com>
2022-06-08 11:23:12 +0700

    Change BasicInfo
    ~Change structure to be based on controller handling current request,
    temp change

--- simplecrmapi/src/main/java/com/simplecrmapi/util/ViewFormatter.java
+++ simplecrmapi/src/main/java/com/simplecrmapi/util/ViewFormatter.java
@@ -2,6 +2,7 @@ package com.simplecrmapi.util;
 
 public class ViewFormatter {
 	public static class BasicInfo{};
-	public static class QueryEmployee extends BasicInfo{};
-	public static class QueryCase extends BasicInfo{};
+	public static class CustomerController extends BasicInfo{};
+	public static class EmployeeController extends BasicInfo{};
+	public static class CaseController extends BasicInfo{};
 }
