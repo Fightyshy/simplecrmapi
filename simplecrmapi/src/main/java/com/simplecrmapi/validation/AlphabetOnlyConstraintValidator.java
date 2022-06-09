35f95b227462257928698231830746c1fd6b84a9
Fightyshy <tharonn@gmail.com>
2022-06-09 16:59:24 +0700

    AlphabetOnly initial commit

---
+++ simplecrmapi/src/main/java/com/simplecrmapi/validation/AlphabetOnlyConstraintValidator.java
@@ -0,0 +1,18 @@
+package com.simplecrmapi.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class AlphabetOnlyConstraintValidator implements ConstraintValidator<AlphabetOnly, String>{
+
+	@Override
+	public void initialize(AlphabetOnly alphabetOnly) {
+		
+	}
+	
+	@Override
+	public boolean isValid(String fieldInput, ConstraintValidatorContext context) {
+		return fieldInput.matches("[a-zA-Z ]+");
+	}
+	
+}
